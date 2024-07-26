d038b911a32a711e20530cb6f3013b288af48249
YUNA329 <ynmerry0329@gmail.com>
2024-07-27 00:23:18 +0900

    add:users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField(max_length=30)
+    
+    class Meta:
+        db_table = 'user'
